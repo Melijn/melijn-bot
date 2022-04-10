5f911f1c9eb99a6fbdb6cf2c4f276023c377cadd
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-04-10 02:09:23 +0200

    feat: dumpmsg command

--- bot/src/main/kotlin/me/melijn/bot/commands/DevExtension.kt
+++ bot/src/main/kotlin/me/melijn/bot/commands/DevExtension.kt
@@ -1,8 +1,14 @@
 package me.melijn.bot.commands
 
+import com.kotlindiscord.kord.extensions.commands.Arguments
+import com.kotlindiscord.kord.extensions.commands.converters.impl.string
 import com.kotlindiscord.kord.extensions.extensions.Extension
+import com.kotlindiscord.kord.extensions.extensions.chatCommand
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
+import dev.kord.common.entity.Snowflake
+import dev.kord.core.behavior.channel.createMessage
+import dev.kord.core.entity.channel.MessageChannel
 import me.melijn.apkordex.command.KordExtension
 
 @KordExtension
@@ -19,6 +25,38 @@ class DevExtension : Extension() {
                     content = ":flushed:"
                 }
             }
+        }
+        chatCommand(::DumpMessageArgs) {
+            name = "dump"
+            description = "dump message content and embed"
+            action {
+                val s = arguments.messageLink.parsed
+                val parts = s.split("/").takeLast(3).map { it.toULong() }
+                val guild = this@DevExtension.kord.getGuild(Snowflake(parts[0]))
+                val channel = guild?.getChannelOrNull(Snowflake(parts[1]))
+                if (channel == null || channel !is MessageChannel) {
+                    this.channel.createMessage("that link is veeery stinky")
+                    return@action
+                }
+                val message = channel.getMessage(Snowflake(parts[2]))
+                this.channel.createMessage {
+                    content = "```\n" +
+                        "content: " + message.content.replace("```", "`\\``") + "\n" +
+                        "embed: [\n" + message.embeds.joinToString(",\n") { it.toString() } +
+                        "\n]\n" +
+                        "attachments: [\n" + message.attachments.joinToString("\n") { it.filename + " - " + it.url } +
+                        "\n]\n" +
+
+                        "```"
+                }
+            }
+        }
+    }
+
+    class DumpMessageArgs : Arguments() {
+        val messageLink = string {
+            name = "messageLink"
+            description = "discord formatted message link"
         }
     }
 }
