6bf26ae76c4a7c9a0d5553efcaea7570da41b04e
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-04-11 17:00:29 +0200

    feat: improve dump util

--- bot/src/main/kotlin/me/melijn/bot/commands/DevExtension.kt
+++ bot/src/main/kotlin/me/melijn/bot/commands/DevExtension.kt
@@ -1,15 +1,21 @@
 package me.melijn.bot.commands
 
 import com.kotlindiscord.kord.extensions.commands.Arguments
-import com.kotlindiscord.kord.extensions.commands.converters.impl.string
+import com.kotlindiscord.kord.extensions.commands.converters.impl.optionalString
 import com.kotlindiscord.kord.extensions.extensions.Extension
 import com.kotlindiscord.kord.extensions.extensions.chatCommand
 import com.kotlindiscord.kord.extensions.extensions.publicSlashCommand
 import com.kotlindiscord.kord.extensions.types.respond
+import com.kotlindiscord.kord.extensions.utils.getJumpUrl
 import dev.kord.common.entity.Snowflake
-import dev.kord.core.behavior.channel.createMessage
 import dev.kord.core.entity.channel.MessageChannel
+import dev.kord.core.event.message.MessageCreateEvent
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import me.melijn.apkordex.command.KordExtension
+import me.melijn.bot.utils.StringsUtil
+import me.melijn.kordkommons.utils.StringUtils
+import org.springframework.boot.ansi.AnsiColor
 
 @KordExtension
 class DevExtension : Extension() {
@@ -30,33 +36,71 @@ class DevExtension : Extension() {
             name = "dump"
             description = "dump message content and embed"
             action {
-                val s = arguments.messageLink.parsed
-                val parts = s.split("/").takeLast(3).map { it.toULong() }
-                val guild = this@DevExtension.kord.getGuild(Snowflake(parts[0]))
-                val channel = guild?.getChannelOrNull(Snowflake(parts[1]))
-                if (channel == null || channel !is MessageChannel) {
-                    this.channel.createMessage("that link is veeery stinky")
-                    return@action
+                val linkArg = arguments.messageLink.parsed
+                val message = if (linkArg != null) {
+                    val parts = linkArg.split("/").takeLast(3).map { it.toULong() }
+                    val guild = this@DevExtension.kord.getGuild(Snowflake(parts[0]))
+                    val channel = guild?.getChannelOrNull(Snowflake(parts[1]))
+                    if (channel == null || channel !is MessageChannel) {
+                        this.channel.createMessage("that link is veeery stinky")
+                        return@action
+                    }
+                    channel.getMessage(Snowflake(parts[2]))
+                } else {
+                    message.referencedMessage!!
                 }
-                val message = channel.getMessage(Snowflake(parts[2]))
-                this.channel.createMessage {
-                    content = "```\n" +
-                        "content: " + message.content.replace("```", "`\\``") + "\n" +
-                        "embed: [\n" + message.embeds.joinToString(",\n") { it.toString() } +
-                        "\n]\n" +
-                        "attachments: [\n" + message.attachments.joinToString("\n") { it.filename + " - " + it.url } +
-                        "\n]\n" +
-
-                        "```"
+                val blue = StringsUtil.ansiFormat(AnsiColor.BLUE)
+                val reset = StringsUtil.ansiFormat(AnsiColor.DEFAULT)
+                var content = ""
+                content += "${blue}content: ${reset}${message.content.replace("```", "'''")}\n"
+                content += "${blue}embed: $reset"
+                if (message.embeds.isNotEmpty()) {
+                    content += "[\n" + message.embeds.joinToString(",\n") {
+                        Json.encodeToString(
+                            it.data
+                        ).replace("```", "'''")
+                    } + "\n]"
                 }
+                content += "\n"
+                content += "${blue}attachments: ${reset}"
+                if (message.attachments.isNotEmpty()) {
+                    content += "[\n" + message.attachments.joinToString("\n") { it.filename + " - " + it.url } + "\n]"
+                }
+                content += "\n"
+                paginator(targetChannel = channel) {
+                    val parts = StringUtils.splitMessage(content)
+                    for (part in parts) {
+                        page {
+                            title = "dumped ${message.id}"
+                            description = "```ansi\n$part```"
+                        }
+                    }
+                }.send()
             }
         }
     }
 
     class DumpMessageArgs : Arguments() {
-        val messageLink = string {
+        val messageLink = optionalString {
             name = "messageLink"
             description = "discord formatted message link"
+
+            validate {
+                val a = value
+                if (a != null) {
+                    failIf("Not a normal message link!") { !".*/\\d+|@me/\\d+/\\d+".toRegex().matches(a) }
+                } else {
+                    val eventObj = context.eventObj
+                    val ref = if (eventObj is MessageCreateEvent) {
+                        eventObj.message.referencedMessage
+                    } else null
+                    failIf("stinkert!") { ref?.getJumpUrl() == null }
+                }
+            }
+        }
+
+        override fun validate() {
+            messageLink.parsed
         }
     }
 }
