14fefa62ef2c9522583df5e0f7a1c71f3d912951
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2022-04-17 16:28:08 +0200

    feat: prefix caching

--- bot/src/main/kotlin/me/melijn/bot/database/manager/PrefixManager.kt
+++ bot/src/main/kotlin/me/melijn/bot/database/manager/PrefixManager.kt
@@ -1,6 +1,9 @@
 package me.melijn.bot.database.manager
 
 import dev.kord.common.entity.Snowflake
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 import me.melijn.ap.injector.Inject
 import me.melijn.gen.PrefixesData
 import me.melijn.gen.database.manager.AbstractPrefixesManager
@@ -9,8 +12,18 @@ import me.melijn.kordkommons.database.DriverManager
 @Inject
 class PrefixManager(driverManager: DriverManager) : AbstractPrefixesManager(driverManager) {
 
-    fun getPrefixes(id: Snowflake): List<PrefixesData> {
-        return getByIndex0(id.value)
+    suspend fun getPrefixes(id: Snowflake): List<PrefixesData> {
+        return getCachedByIndex0(id.value)
     }
 
+    private suspend fun getCachedByIndex0(id: ULong): List<PrefixesData> {
+        val key = "melijn:prefixes:${id}"
+        driverManager.getCacheEntry(key, 5)?.run {
+            Json.decodeFromString<List<PrefixesData>>(this)
+        }
+        val cachable = getByIndex0(id)
+        val cachableStr = Json.encodeToString(cachable)
+        driverManager.setCacheEntry(key, cachableStr, 5)
+        return cachable
+    }
 }
