2d837fd453b98620ab58584cde09bd11dbbcaddb
pajamaman18 <pajamaman18@gmail.com>
2022-10-29 23:12:36 +0200

    added beg translation, implemented beg without any cooldown

--- bot/src/main/kotlin/me/melijn/bot/commands/EconomyExtension.kt
+++ bot/src/main/kotlin/me/melijn/bot/commands/EconomyExtension.kt
@@ -115,6 +115,22 @@ class EconomyExtension : Extension() {
                 }
             }
         }
+
+        publicSlashCommand {
+            name = "beg"
+            description = "Beg for a small amount of mel, max once every hour"
+
+            action {
+                //implement hourly beg cooldown
+                val receivedAmount = Random.nextInt(11)
+                val recipient = balanceManager.get(user.id)
+                recipient.balance += receivedAmount
+                balanceManager.store(recipient)
+                respond {
+                    content = tr("balance.beg",receivedAmount)
+                }
+            }
+        }
     }
 
     private suspend fun PublicSlashCommandContext<*>.flipAmount(
