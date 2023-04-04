20228d1db0070fa5bd2cb60c5c14d03aa850d058
ToxicMushroom <32853531+ToxicMushroom@users.noreply.github.com>
2023-04-05 01:52:25 +0200

    refactor: rename StartupService.kt to StartupListener because it doesn't do repeating tasks

--- bot/src/main/kotlin/me/melijn/bot/services/StartupListener.kt
+++ bot/src/main/kotlin/me/melijn/bot/services/StartupListener.kt
@@ -14,7 +14,7 @@ import net.dv8tion.jda.api.events.session.ReadyEvent
 import net.dv8tion.jda.api.sharding.ShardManager
 
 @Inject(true)
-class StartupService {
+class StartupListener {
 
     val logger by Log
 
